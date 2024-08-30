92d3def50cc82eede81b7a26c02350d7d8958722
M.ngugi <mngugi7@gmail.com>
2024-08-30 13:15:23 +0300

    Update scrapTwitter.py

--- Notes/Notes/scrapTwitter.py
+++ Notes/Notes/scrapTwitter.py
@@ -1,8 +1,30 @@
 from pathlib import Path
+from typing import Any, Iterable
 
 
 import scrapy
+from scrapy.http import Response
 
 
 class twitterSpider( scrapy.Spider):
+    name = "twitter"
+    
+    
+    def start_requests(self):
+        urls = [
+            "https://x.com/home"
+            
+            
+        ] 
+        for url in urls:
+            yield scrapy.Request( url=url, callback=self.parse)
+            
+            
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f"twitter-{page}.html"
+        Path(filename).write_bytes(response.body)
+        self.log(f"Saved file {filename}")
+        
+    
     
